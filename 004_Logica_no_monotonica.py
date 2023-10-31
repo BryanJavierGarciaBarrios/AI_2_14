c6c67b7d2b408856b3d1995c57f84d74ee3f5b0e
BryanJavierGarciaBarrios <145368570+BryanJavierGarciaBarrios@users.noreply.github.com>
2023-10-31 12:22:51 -0600

    Logica_no_monotonica

---
+++ 004_Logica_no_monotonica.py
@@ -0,0 +1,33 @@
+import pyke
+
+# Crear un motor de inferencia
+engine = pyke.get_default_engine()
+
+# Definir hechos iniciales
+engine.activate('facts')
+
+# Definir una regla inicial
+engine.activate('rules')
+
+# Definir una nueva regla no monótona
+engine.add_unsourced('facts', ('Puede_volar', 'Pingüino'), user_data='puede no volar')
+
+# Consulta para verificar si un pingüino puede volar
+consulta = "Puede_volar ?x"
+engine.prove_1_goal('facts', consulta)
+
+# Imprimir los resultados
+for plan in engine.plans:
+    print(plan.__str__())
+
+# Retractar la nueva regla no monótona
+engine.remove_unsourced('facts', ('Puede_volar', 'Pingüino'), user_data='puede no volar')
+
+# Consulta nuevamente
+engine.reset()  # Reiniciar el motor
+engine.activate('facts')
+engine.prove_1_goal('facts', consulta)
+
+# Imprimir los resultados
+for plan in engine.plans:
+    print(plan.__str__())
